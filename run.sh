8ec733a3839d983d28f6667d36c217837291bcd9
wsguede <wsguede@gmail.com>
2017-01-18 16:54:56 +0000

    small fixes

--- run.sh
+++ run.sh
@@ -2,10 +2,11 @@
 # @author Bill Guedel <wsguede@gmail.com>
 #
 # This is what should be run on a fresh install
-@echo off
 
 . ./.rm_bloat
 . ./.add
 . ./.setup
 
 echo "READY! Dont forget to set up your apps!"
+echo "To remove this helper please run"
+echo "  cd .. && rm -rf $PWD"
