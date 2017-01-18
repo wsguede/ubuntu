59feb521244789a0a74de385234159ddbc0cb54c
wsguede <wsguede@gmail.com>
2017-01-18 02:26:51 +0000

    adding run file, renaming to hidden files

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# @author Bill Guedel <wsguede@gmail.com>
+#
+# This is what should be run on a fresh install
+@echo off
+
+. ./.rm_bloat
+. ./.add
+. ./.setup
+
+echo "READY! Dont forget to set up your apps!"
